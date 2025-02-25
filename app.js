fa9301e00a2f136988014cdd33133cb94b8bf564
SAI-IT\tu.vancong <tu.van@sai-digital.com>
2025-02-25 18:22:02 +0700

    app api ping

--- app.js
+++ app.js
@@ -32,6 +32,16 @@ app.get("/app/image-optimizer/health", (req, res) => {
   res.status(200).send(response);
 });
 
+//APP APIs
+app.get("/app/v1/ping", (req, res) => {
+  const message = "Server running!";
+  const version = "NodeJS " + process.versions.node;
+  const response = [message, version].join("\n");
+
+  res.status(200).send(response);
+});
+
+
 // Khởi tạo server HTTP và lắng nghe
 const server = http.createServer(app);
 
